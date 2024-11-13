3dc71410ae2ef70179d344b83bb5e3f8f0f8d7d5
jlib245 <jlib245@naver.com>
2024-11-13 15:51:52 +0900

    sdff'

--- cppFolder/main.cpp
+++ cppFolder/main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
-    int i = 9;
-    double f = 3.141592;
-    int *pi;
-    double *pf = &f;
-
-    i = static_cast<int>(f);
-    pi = (int*)pf;
-    cout << *pi;
+    cout << "hello, world";
     return 0;
-
 }
