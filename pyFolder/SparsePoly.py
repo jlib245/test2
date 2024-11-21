44e36135c8000bb0294e88f3c257c7167bcfa4f4
jlib245 <jlib245@naver.com>
2024-11-21 18:42:29 +0900

    really completed

--- pyFolder/SparsePoly.py
+++ pyFolder/SparsePoly.py
@@ -1,6 +1,5 @@
 from LinkedList import Node, LinkedList
 
-# 다항식의 항을 나타내는 클래스
 class Term:
     def __init__(self, expo, coef):
         self.expo = expo  # 지수
@@ -9,14 +8,14 @@ class Term:
     def __str__(self):
         return f"{self.coef}x^{self.expo}"
 
-# LinkedList를 상속받는 SparsePoly 클래스
+# LinkedList를 상속
 class SparsePoly(LinkedList):
     def __init__(self):
-        super().__init__()  # LinkedList 초기화
+        super().__init__()  
 
     def getNode(self, pos):
         if pos < 0: 
-            return None  # 잘못된 위치는 None 반환
+            return None  
         
         node = self.head
         while pos > 0 and node != None:
@@ -28,37 +27,32 @@ class SparsePoly(LinkedList):
         if pos < 0:
             raise IndexError("Position must be non-negative.")
         
-        if pos == 0:  # 맨 앞에 삽입
+        if pos == 0: 
             self.head = Node(elem, self.head)
             return
 
-        # 이전 노드를 가져옴
         before = self.getNode(pos - 1)
-        if before == None:  # 이전 노드가 없으면 예외 처리
-            raise IndexError("Position out of bounds.")
+        if before == None:  
+            return
 
-        # 새 노드 생성 및 삽입
         new_node = Node(elem, before.link)
         before.link = new_node
 
     def addTerm(self, coef, expo):
         if coef == 0:
-            return  # 계수가 0이면 추가하지 않음
+            return  
 
-        # 현재 노드를 순회하며 삽입 위치를 찾음
         node = self.head
         pos = 0
         while node != None and node.data.expo > expo:
             node = node.link
             pos += 1
 
-        # 동일한 지수가 존재하면 계수를 더함
         if node != None and node.data.expo == expo:
             node.data.coef += coef
-            if node.data.coef == 0:  # 계수가 0이 되면 해당 항 삭제
+            if node.data.coef == 0:  
                 self.delete(pos)
         else:
-            # 새 항을 삽입
             self.insert(pos, Term(expo, coef))
 
     def evaluate(self, x):
@@ -90,18 +84,16 @@ class SparsePoly(LinkedList):
         print(s, end=' ')
         print(" + ".join(terms) if terms else "0")
     
-    # Big-O : O(n^2)
+    # Big-O : O(m^2)
     def __add__(self, other):
         result = SparsePoly()
 
-        # 첫 번째 다항식 항들을 결과에 추가
         node = self.head
         while node != None:
             term = node.data
             result.addTerm(term.coef, term.expo)
             node = node.link
 
-        # 두 번째 다항식 항들을 결과에 추가
         node = other.head
         while node != None:
             term = node.data
@@ -112,7 +104,6 @@ class SparsePoly(LinkedList):
     
     def __eq__(self, other):
 
-        # 두 다항식을 순회하면서 비교
         node1 = self.head
         node2 = other.head
 
@@ -120,12 +111,22 @@ class SparsePoly(LinkedList):
             term1 = node1.data
             term2 = node2.data
 
-            # 지수 또는 계수가 다르면 False
             if term1.expo != term2.expo or term1.coef != term2.coef:
                 return False
 
             node1 = node1.link
             node2 = node2.link
 
-        # 두 다항식이 모두 끝났으면 True, 아니면 False
         return node1 == None and node2 == None
+    
+    def degree(self) :
+        return self.head.data.expo
+    
+    def coef(self) :
+        terms = []
+        node = self.head
+        while node != None:
+            terms.append(node.data.coef)
+            node = node.link
+
+        return terms
